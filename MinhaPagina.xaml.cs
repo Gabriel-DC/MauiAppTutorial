b9bb32282822bbc26f38d12083a611e3fcd00a14
Gabriel Almeida <57327840+Gabriel-DC@users.noreply.github.com>
2023-11-15 11:38:09 -0300

    Data Binding I

--- MinhaPagina.xaml.cs
+++ MinhaPagina.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppTutorial.Models;
+
 namespace MauiAppTutorial;
 
 public partial class MinhaPagina : ContentPage
@@ -7,8 +9,38 @@ public partial class MinhaPagina : ContentPage
 		InitializeComponent();
 	}
 
-	private async void Button_OnClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new MainPage());
-	}
+  //  private void BtnBind_Clicked(object sender, EventArgs e)
+  //  {
+		//Produto product = new()
+		//{
+		//	Nome = "Iphone 5",
+		//	Preco = 5000m,
+		//	Estoque = 5
+		//};
+
+		//Binding nameBinding = new()
+		//{
+		//	Source = product,
+		//	Path = "Nome",
+		//	StringFormat = "Produto: {0}"
+		//};
+
+  //      Binding priceBinding = new()
+  //      {
+  //          Source = product,
+  //          Path = "Preco",
+  //          StringFormat = "Preco: {0}"
+  //      };
+
+  //      Binding stockBinding = new()
+  //      {
+  //          Source = product,
+  //          Path = "Estoque",
+  //          StringFormat = "Estoque: {0}"
+  //      };
+
+  //      LblNome.SetBinding(Label.TextProperty, nameBinding);
+  //      LblPreco.SetBinding(Label.TextProperty, priceBinding);
+  //      LblEstoque.SetBinding(Label.TextProperty, stockBinding);
+  //  }
 }
